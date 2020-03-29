67454df5f95357bf81225b3283639bebbbad46de
Mikhail Babaev <mmbabaev@gmail.com>
2020-03-30 02:43:39 +0300

    add TranslatePresenterTests

--- SkyEngTaskTests/Mocks/Generated.swift
+++ SkyEngTaskTests/Mocks/Generated.swift
@@ -41,6 +41,72 @@ class NetworkRequestMock: NetworkRequest {
     }
 
 }
+class TranslateRouterMock: TranslateRouter {
+
+    //MARK: - routeToWord
+
+    var routeToWordCallsCount = 0
+    var routeToWordCalled: Bool {
+        return routeToWordCallsCount > 0
+    }
+    var routeToWordReceivedWord: Word?
+    var routeToWordReceivedInvocations: [Word] = []
+    var routeToWordClosure: ((Word) -> Void)?
+
+    func routeToWord(_ word: Word) {
+        routeToWordCallsCount += 1
+        routeToWordReceivedWord = word
+        routeToWordReceivedInvocations.append(word)
+        routeToWordClosure?(word)
+    }
+
+}
+class TranslateViewMock: TranslateView {
+
+    //MARK: - displayCells
+
+    var displayCellsCallsCount = 0
+    var displayCellsCalled: Bool {
+        return displayCellsCallsCount > 0
+    }
+    var displayCellsReceivedCellModels: [WordCellViewModel]?
+    var displayCellsReceivedInvocations: [[WordCellViewModel]] = []
+    var displayCellsClosure: (([WordCellViewModel]) -> Void)?
+
+    func displayCells(_ cellModels: [WordCellViewModel]) {
+        displayCellsCallsCount += 1
+        displayCellsReceivedCellModels = cellModels
+        displayCellsReceivedInvocations.append(cellModels)
+        displayCellsClosure?(cellModels)
+    }
+
+    //MARK: - displayError
+
+    var displayErrorCallsCount = 0
+    var displayErrorCalled: Bool {
+        return displayErrorCallsCount > 0
+    }
+    var displayErrorClosure: (() -> Void)?
+
+    func displayError() {
+        displayErrorCallsCount += 1
+        displayErrorClosure?()
+    }
+
+    //MARK: - endLoading
+
+    var endLoadingCallsCount = 0
+    var endLoadingCalled: Bool {
+        return endLoadingCallsCount > 0
+    }
+    var endLoadingClosure: (() -> Void)?
+
+    func endLoading() {
+        endLoadingCallsCount += 1
+        endLoadingClosure?()
+    }
+
+}
 class WordServiceMock: WordService {
 
     //MARK: - search
@@ -59,6 +125,39 @@ class WordServiceMock: WordService {
         searchPagePageSizeCallbackReceivedArguments = (search: search, page: page, pageSize: pageSize, callback: callback)
         searchPagePageSizeCallbackReceivedInvocations.append((search: search, page: page, pageSize: pageSize, callback: callback))
         return searchPagePageSizeCallbackClosure.map({ $0(search, page, pageSize, callback) }) ?? searchPagePageSizeCallbackReturnValue
+    }
+
+}
+class WordWorkerMock: WordWorker {
+
+    //MARK: - loadNextPage
+
+    var loadNextPageSearchCallbackCallsCount = 0
+    var loadNextPageSearchCallbackCalled: Bool {
+        return loadNextPageSearchCallbackCallsCount > 0
+    }
+    var loadNextPageSearchCallbackReceivedArguments: (search: String, callback: ResultCallback<[Word]>)?
+    var loadNextPageSearchCallbackReceivedInvocations: [(search: String, callback: ResultCallback<[Word]>)] = []
+    var loadNextPageSearchCallbackClosure: ((String, @escaping ResultCallback<[Word]>) -> Void)?
+
+    func loadNextPage(search: String, callback: @escaping ResultCallback<[Word]>) {
+        loadNextPageSearchCallbackCallsCount += 1
+        loadNextPageSearchCallbackReceivedArguments = (search: search, callback: callback)
+        loadNextPageSearchCallbackReceivedInvocations.append((search: search, callback: callback))
+        loadNextPageSearchCallbackClosure?(search, callback)
+    }
+
+    //MARK: - reset
+
+    var resetCallsCount = 0
+    var resetCalled: Bool {
+        return resetCallsCount > 0
+    }
+    var resetClosure: (() -> Void)?
+
+    func reset() {
+        resetCallsCount += 1
+        resetClosure?()
     }
 
 }
